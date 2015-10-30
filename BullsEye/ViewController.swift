f16162fd88c4e40d4df7df5d485724f9b3832f86
Vasyl Kotsiuba <clic@ukr.net>
2015-10-30 17:03:18 +0200

    added autoLayout constraints

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -12,29 +12,89 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var targetLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var roundLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     var currentValue: Int = 0
-    var targetValue: Int = 0
+    //var targetValue: Int = 0
+    //OR
+    var targetValue = 0
+    var score = 0
+    var round = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //prinrln ("i cant wait to start")
-        currentValue = lroundf(slider.value)
+        //currentValue = lroundf(slider.value)
+        //targetValue = 1 + Int(arc4random_uniform(100))
         
-        targetValue = 1 + Int(arc4random_uniform(100))
+        startNewRound()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Game logic
+    
+    func startNewRound () {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        //slider.value = Float(currentValue)
+        slider.setValue(Float(currentValue), animated: true)
+        round++
+        
+        //Update UI
+        self.updateLabels()
+    }
+    
+    // MARK: - Update UI
+    func updateLabels () {
+        self.targetLabel.text = String(self.targetValue)
+        // OR
+        //targetLabel.text = "\(targetValue)"
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
 
+    // MARK: - Actions
     @IBAction func showAlert() {
         
+        /*
+        var difference: Int
+        if currentValue > targetValue {
+            difference = currentValue - targetValue
+        } else if targetValue > currentValue {
+            difference = targetValue - currentValue
+        } else {
+            difference = 0
+        }
+        */
+        
+        //OR
+        /*
+        var difference = currentValue - targetValue
+        if difference < 0 {
+            difference *= -1
+        }
+        */
+        
+        //OR
+        let difference = abs(currentValue - targetValue)
+        let points = 100 - difference
+        score += points
+        
+        /*
         let message = "The value of the slider is: \(currentValue)" +
         "\nThe target value is: \(targetValue)"
-        
+        + "\nThe difference is: \(difference)"
+        */
+    
+        let message = "You scored \(points) points"
         let alert = UIAlertController (title: "Hello, Word",
                                     message: message,
                                 preferredStyle: .Alert)
@@ -45,7 +105,7 @@ class ViewController: UIViewController {
         
         presentViewController(alert, animated: true, completion: nil)
         
-        
+        startNewRound()
     }
     
     @IBAction func sliderMoved(slider: UISlider) {
